b5b4679d587f3c9cd959eff497bb6c3887e77386
Umang <umangagarwal345@gmail.com>
2022-06-28 22:33:24 +0530

    Exposed CRUD Enpoints in Redis Controller

---
+++ src/main/java/com/umang345/springdataredis/controller/RedisController.java
@@ -0,0 +1,36 @@
+package com.umang345.springdataredis.controller;
+
+import com.umang345.springdataredis.entity.Product;
+import com.umang345.springdataredis.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class RedisController
+{
+    @Autowired
+    private ProductDao dao;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable int id)   {
+        return dao.deleteProduct(id);
+    }
+}
